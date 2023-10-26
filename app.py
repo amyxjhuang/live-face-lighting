342a1b92f35dbf510d7b44d1f4fc8282b7766e72
Amy Huang <amyxjhuang@berkeley.edu>
2023-10-25 23:40:36 -0400

    add center point

--- app.py
+++ app.py
@@ -3,26 +3,33 @@ import sys
 
 faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-video_capture = cv2.VideoCapture(0) # video sourrce
+video_capture = cv2.VideoCapture(0) # video source
 
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
+    frame = cv2.flip(frame, 1)
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
     faces = faceCascade.detectMultiScale(
         gray,
         scaleFactor=1.1,
-        minNeighbors=5,
+        minNeighbors=10,
         minSize=(30, 30),
         flags=cv2.CASCADE_SCALE_IMAGE
     )
+    height, width, channels = frame.shape
+    center_coordinates = (width // 2, height // 2)
 
+    green_color = (0, 255, 0)
+    pink_color = (255, 0 , 255)
+    border_thickness = 2
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), green_color, border_thickness)
 
+        face_midpoint = (x + w // 2,  y + h // 2)
+        cv2.line(frame, center_coordinates, face_midpoint, pink_color, border_thickness)
     # Display the resulting frame
     cv2.imshow('Video', frame)
 
